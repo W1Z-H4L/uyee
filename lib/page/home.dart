f1726353b00b74baa156116dc97d6f81957e09c9
aditiyagilang <aditiyagilang031@gmail.com>
2022-10-27 19:11:52 +0700

    edit by adit

--- lib/page/home.dart
+++ lib/page/home.dart
@@ -1,5 +1,3 @@
-import 'dart:html';
-
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:login/shared/theme.dart';
@@ -8,43 +6,126 @@ class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return Card(
-      elevation: 30,
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
-      color: subtitleColor2,
-      child: InkWell(
-        onTap: () {},
-        splashColor: greenColor,
-        highlightColor: greenColor,
-        hoverColor: greenColor,
-        borderRadius: BorderRadius.circular(40),
-        child: Center(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              ClipRRect(
-                borderRadius: BorderRadius.circular(30),
-                child: Image.asset('assets/icon_Schedule.png'),
+    return GridView.count(
+        padding: const EdgeInsets.all(25),
+        crossAxisCount: 2,
+        children: <Widget>[
+          Card(
+            elevation: 30,
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(40)),
+            color: subtitleColor2,
+            child: InkWell(
+              onTap: () {},
+              splashColor: greenColor,
+              highlightColor: greenColor,
+              hoverColor: greenColor,
+              borderRadius: BorderRadius.circular(40),
+              child: Center(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(40),
+                      child: Image.asset('assets/icon_Schedule.png'),
+                    ),
+                    Text(
+                      'Schedule',
+                      style: karen,
+                    )
+                  ],
+                ),
               ),
-              Text(
-                'Schedule',
-                style: karen,
-              )
-            ],
+            ),
+            margin: EdgeInsets.only(left: 15, right: 15, top: 15),
           ),
-          // mainAxisSize: MainAxisSize.min,
-          // children: const <Widget>[
-          //   Text(
-          //     'Schedule',
-          //     style: TextStyle(
-          //       color: Colors.white,
-          //       fontSize: 12,
-          //     ),
-          //   ),
-          // ],
-        ),
-      ),
-      margin: EdgeInsets.only(left: 20, right: 20, top: 7),
-    );
+          Card(
+            elevation: 30,
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(40)),
+            color: subtitleColor2,
+            child: InkWell(
+              onTap: () {},
+              splashColor: greenColor,
+              highlightColor: greenColor,
+              hoverColor: greenColor,
+              borderRadius: BorderRadius.circular(40),
+              child: Center(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(40),
+                      child: Image.asset('assets/icon_activity.png'),
+                    ),
+                    Text(
+                      'Activity',
+                      style: karen,
+                    )
+                  ],
+                ),
+              ),
+            ),
+            margin: EdgeInsets.only(left: 15, right: 15, top: 15),
+          ),
+          Card(
+            elevation: 30,
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(40)),
+            color: subtitleColor2,
+            child: InkWell(
+              onTap: () {},
+              splashColor: greenColor,
+              highlightColor: greenColor,
+              hoverColor: greenColor,
+              borderRadius: BorderRadius.circular(40),
+              child: Center(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(40),
+                      child: Image.asset('assets/icon_padicare.png'),
+                    ),
+                    Text(
+                      'Padi Care',
+                      style: karen,
+                    )
+                  ],
+                ),
+              ),
+            ),
+            margin: EdgeInsets.only(left: 15, right: 15, top: 15),
+          ),
+          Card(
+            elevation: 30,
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(40)),
+            color: subtitleColor2,
+            child: InkWell(
+              onTap: () {},
+              splashColor: greenColor,
+              highlightColor: greenColor,
+              hoverColor: greenColor,
+              borderRadius: BorderRadius.circular(40),
+              child: Center(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(40),
+                      child: Image.asset('assets/icon_varietedpadi.png'),
+                    ),
+                    Text(
+                      'Varietas Padi',
+                      style: karen,
+                    )
+                  ],
+                ),
+              ),
+            ),
+            margin: EdgeInsets.only(left: 15, right: 15, top: 15),
+          ),
+        ]);
   }
 }
